a4ea5092877c19d4c0054a8945573f7a99989ec8
Nahid-Hassan <nahid.cseru@gmail.com>
2020-09-24 18:24:28 +0600

    basic dash_apps tutorial datacamp

---
+++ python3/dash_apps/dash_for_beginners_datacamp.py
@@ -0,0 +1,59 @@
+"""
+pip install dash==0.21.1  
+pip install dash-renderer==0.13.0  
+pip install dash-html-components==0.11.0
+pip install dash-core-components==0.23.0  
+pip install plotly --upgrade
+"""
+
+# Dash App Layout
+# First Part is layout and second part is interactivity
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash()
+
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+
+app = dash.Dash()
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+    html.H1(
+        children='Hello Dash',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
+    html.Div(children='Dash: A web application framework for Python.', style={
+        'textAlign': 'center',
+        'color': colors['text']
+    }),
+    dcc.Graph(
+        id='Graph1',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'plot_bgcolor': colors['background'],
+                'paper_bgcolor': colors['background'],
+                'font': {
+                    'color': colors['text']
+                }
+            }
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
