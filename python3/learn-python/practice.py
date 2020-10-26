5b8355d7e6bbe38d1791d0fa321c9ad17418614e
Nahid-Hassan <nahid.cseru@gmail.com>
2020-10-26 16:05:24 +0600

    practice.py file is added

---
+++ python3/learn-python/practice.py
@@ -0,0 +1,29 @@
+import time
+
+start_time = time.time()
+
+
+
+
+
+
+# write your code here
+
+names = ['Nahid', "Meem", "Mahin", "Bristy"]
+
+# for name in names:
+#     # print("Hello there, " + name)
+#     print(''.join(["Hello there ", name]))
+
+print(', '.join(names))
+
+# end of code
+
+
+
+
+
+
+
+end_time =time.time()
+print(start_time - end_time)
