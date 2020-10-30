867dd43ec6fea248e53d7f8381e20b069b4a2fde
Nahid-Hassan <nahid.cseru@gmail.com>
2020-10-30 10:34:18 +0600

    simple calculator part#1

---
+++ python3/python-gui/calculator.py
@@ -0,0 +1,60 @@
+from tkinter import *
+
+# start
+root = Tk()
+
+# set title
+root.title("Simple Calculator")
+
+# entry box
+entry = Entry(root, width=35, borderwidth=3)
+
+'''
+columnspan = 2 is a property that declare this widget is span with 2 columns.  
+'''
+
+entry.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+
+
+def button_click(number):
+    # entry.delete(0, END)
+    entry.insert(0, number)
+
+
+# Define Buttons
+button_1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
+button_2 = Button(root, text="2", padx=40, pady=20, command=lambda: button_click(2))
+button_3 = Button(root, text="3", padx=40, pady=20, command=lambda: button_click(3))
+button_4 = Button(root, text="4", padx=40, pady=20, command=lambda: button_click(4))
+button_5 = Button(root, text="5", padx=40, pady=20, command=lambda: button_click(5))
+button_6 = Button(root, text="6", padx=40, pady=20, command=lambda: button_click(6))
+button_7 = Button(root, text="7", padx=40, pady=20, command=lambda: button_click(7))
+button_8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8))
+button_9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
+button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
+
+button_add = Button(root, text="+", padx=39, pady=20, command=lambda: button_click())
+button_equal = Button(root, text="=", padx=91, pady=20, command=lambda: button_click())
+button_clear = Button(root, text="Clear", padx=79, pady=20, command=lambda: button_click())
+
+# Put the buttons on the screen
+button_1.grid(row=3, column=0)
+button_2.grid(row=3, column=1)
+button_3.grid(row=3, column=2)
+
+button_4.grid(row=2, column=0)
+button_5.grid(row=2, column=1)
+button_6.grid(row=2, column=2)
+
+button_7.grid(row=1, column=0)
+button_8.grid(row=1, column=1)
+button_9.grid(row=1, column=2)
+
+button_0.grid(row=4, column=0)
+
+button_add.grid(row=5, column=0)
+button_equal.grid(row=5, column=1, columnspan=2)
+button_clear.grid(row=4, column=1, columnspan=2)
+
+# end
+root.mainloop()
