f98d0f9093c322bb7428a908b493e4d7dbf6e2f0
Nahid-Hassan <nahid.cseru@gmail.com>
2020-11-05 16:52:54 +0600

    add subprocess

---
+++ python3/learn-python/subprocess.py
@@ -0,0 +1,25 @@
+import subprocess
+import time
+from pprint import pprint
+from datetime import datetime
+from datetime import timedelta
+import os
+
+# out = subprocess.run(['ls', '-la'])
+
+# print(out.args)
+# print(out.returncode)
+
+# out = subprocess.run(['ls'], capture_output=True)
+# out = out.stdout # return a byte string
+# out = out.decode('utf-8') # convert bytestring to string using utf-8 encoding
+
+# pprint(out.split('\n'))
+
+# out = subprocess.run(['ls', '-al'], capture_output=True)
+
+# print(out.stdout.decode())
+
+# out = subprocess.run(['ls', '-al'], capture_output=True, text=True)
+
+# print(out.stdout)
